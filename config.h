f8f9e308852bedd0f6fe44d02d672625eafc4a5d
s73331 <s73331@htw-dresden.de>
2016-01-11 21:23:28 +0100

    config.h typos

--- config.h
+++ config.h
@@ -6,7 +6,7 @@ for Client/ Server
 #define P_MESSAGE_3 " address\tIP address on which to bind. (default: unspecified address)\n"
 #define P_MESSAGE_4 " port\t\tPort on which to bind. (default: %s)\n"
 #define P_MESSAGE_5 "Hit Ctrl-C to terminate\n"
-#define P_MESSAGE_6 "\n%s {-5 server] [-p port] [-f file] [—w window_size]\n\n"
+#define P_MESSAGE_6 "\n%s [-s server] [-p port] [-f file] [-w window_size]\n\n"
 #define P_MESSAGE_7 " server\tName or IP address on which to connect. (default: %s)\n"
 #define P_MESSAGE_8 " port\t\tPort on which to connect. (default: %s)\n"
 #define P_MESSAGE_9 " fi1e\t\tFile wich to recieve. (default: empty)\n"
