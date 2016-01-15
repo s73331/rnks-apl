c954d88387037073871c25c677c832212104a01c
s73331 <s73331@htw-dresden.de>
2016-01-15 19:18:06 +0100

    config.h compatibility

--- config.h
+++ config.h
@@ -1,13 +1,13 @@
 /*
 for Client/ Server
 */
-#define P_MESSAGE_1 "\nHow to use.\n"
-#define P_MESSAGE_2 "\n%s [-a adress] [-p port]\n\n"
-#define P_MESSAGE_3 " address\tIP address on which to bind. (default: unspecified address)\n"
-#define P_MESSAGE_4 " port\t\tPort on which to bind. (default: %s)\n"
-#define P_MESSAGE_5 "Hit Ctrl-C to terminate\n"
-#define P_MESSAGE_6 "\n%s [-s server] [-p port] [-f file] [-w window_size]\n\n"
-#define P_MESSAGE_7 " server\tName or IP address on which to connect. (default: %s)\n"
-#define P_MESSAGE_8 " port\t\tPort on which to connect. (default: %s)\n"
-#define P_MESSAGE_9 " file\t\tFile wich to recieve. (default: empty)\n"
-#define P_MESSAGE_10 " window_size\t\twindow size of Reliable MC Protocol (maximum: 10).\n"
+#define P_Message_1 "\nHow to use.\n"
+#define P_Message_2 "\n%s [-a adress] [-p port]\n\n"
+#define P_Message_3 " address\tIP address on which to bind. (default: unspecified address)\n"
+#define P_Message_4 " port\t\tPort on which to bind. (default: %s)\n"
+#define P_Message_5 "Hit Ctrl-C to terminate\n"
+#define P_Message_6 "\n%s [-s server] [-p port] [-f file] [-w window_size]\n\n"
+#define P_Message_7 " server\tName or IP address on which to connect. (default: %s)\n"
+#define P_Message_8 " port\t\tPort on which to connect. (default: %s)\n"
+#define P_Message_9 " file\t\tFile wich to recieve. (default: empty)\n"
+#define P_Message_10 " window_size\t\twindow size of Reliable MC Protocol (maximum: 10).\n"
