0f97ddd19fb661dd56d06d705e8463134004c936
s73331 <s73331@htw-dresden.de>
2016-01-15 18:36:59 +0100

    changed DEFAULT_SERVER, martin

--- toUdp.h
+++ toUdp.h
@@ -1,7 +1,8 @@
 /*
 Could be used by Client & Server
 */
-#define DEFAULT_SERVER      "FF11::10"       //... will use the loopback interface
+//#define DEFAULT_SERVER      "FF11::10"       //... will use the loopback interface
+#define DEFAULT_SERVER	    "FF02::10"
 #define DEFAULT_FAMILY      AF_INET6         //... accept IPv6
 #define DEFAULT_SOCKTYPE    SOCK_DGRAM       // UDP | SOCK_STREAM ... TCP
 #define DEFAULT_PORT        "50000"          // 50000 ... test port;
