e86aac4deb635e673339eb5f80ca7426996f9d53
martin <s73331@htw-dresden.de>
2016-01-16 23:16:29 +0100

    typo, martin

--- data.h
+++ data.h
@@ -4,7 +4,7 @@
 #define TIMEOUT_INT 300             // in Milliseconds
 #define TIMEOUT 3                   // must be a multiple of TIMEOUT_INT
 #define MAX_WINDOW 10               // maximum windows size
-#define MAX_SEQNR 2*MAX_WINDON-1    /* maximum sequence number --> real maximum sequence number
+#define MAX_SEQNR 2*MAX_WINDOW-1    /* maximum sequence number --> real maximum sequence number
                                        is 2 times of the choosen real window size!!*/
 #define MAX_BUFFER 2*MAX_WINDOW     // packets must be stored for retransmission
 
