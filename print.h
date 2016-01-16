779ea4b1d67d7109f149781d332aca60f79354f3
martin <s73331@htw-dresden.de>
2016-01-16 21:48:05 +0100

    formatting

--- print.h
+++ print.h
@@ -4,6 +4,6 @@
 void printAns(struct answer answ, int sent);
 
 /*
-sent shall be non-zero if sent and zero if received
+    sent shall be non-zero if sent and zero if received
 */
 void printReq(struct request req, int sent);
