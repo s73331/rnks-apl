db04a363f9a36bcb35bf024617b318630bb1be01
martin <s73331@htw-dresden.de>
2016-01-17 13:03:45 +0100

    added flag ignored, made full words instead of words, martin

--- print.c
+++ print.c
@@ -5,8 +5,8 @@
 void printAns(struct answer answ, int sent)
 {
     printf("Answer\tNo: %i\ttype: %c\tstatus: ", answ.SeNo, answ.AnswType);
-    if (sent) printf("S");
-    else printf("R");
+    if (sent) printf("Sent");
+    else printf("Received");
     printf("\n");
 }
 void printReq(struct request req, int flag)
@@ -14,13 +14,15 @@ void printReq(struct request req, int flag)
     printf("Request\tNo: %i\ttype: %c\tstatus: ", req.SeNr, req.ReqType);
     switch (flag)
     {
-    case 0: printf("R");
+    case 0: printf("Received");
         break;
-    case 1: printf("S");
+    case 1: printf("Sent\t");
         break;
-    case 2: printf("C");
+    case 2: printf("Cached\t");
         break;
-    case 3: printf("U");
+    case 3: printf("Uncached");
+        break;
+    case 4: printf("Ignored\t");
         break;
     default:
         fprintf(stderr, "passed invalid flag to printReq(): %i", flag);
