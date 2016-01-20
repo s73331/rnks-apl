2190711f55ef61cbd6750230b88acbb96a88b97c
martin <s73331@htw-dresden.de>
2016-01-20 23:12:58 +0100

    new local.h

--- local.h
+++ local.h
@@ -1,2 +1,8 @@
 #define FILE_TO_READ "\\Users\\Martin\\LoremIpsum.txt"
 #define FILE_TO_WRITE "\\Users\\Martin\\output.txt"
+#define IPV6MR_INTERFACE INADDR_ANY
+//#define IPV6MR_INTERFACE 5
+#define LEVEL SOL_SOCKET
+//#define LEVEL IPPROTO_IPV6
+#define OPTNAME SO_REUSEADDR
+//#define OPTNAME IPV6_MULTICAST_IF
